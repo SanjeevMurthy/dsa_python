82435d42553c86b75a99da0b1855f15a907b21f4
Sanjeev Murthy <sanjeevmurthy@Sanjeevs-MacBook-Air.local>
2024-09-04 00:43:11 +0530

    Fix: use integer division and return outside while loop if number not found

--- binary_search.py
+++ binary_search.py
@@ -6,24 +6,25 @@ def binary_search(list_of_num, num_to_search):
 
     while low <= high:
         print(f"Step: {step} | Number to Search: {num_to_search} | Low: {low}  | High: {high}  ")
-        mid = round(low + high / 2)
+        mid = round(low + high // 2)
         print(f"Mid: {mid}")
         step = step + 1
         if list_of_num[mid] == num_to_search:
             return mid
         elif list_of_num[mid] < num_to_search:
             low = mid + 1
-        elif list_of_num[mid] > num_to_search:
-            high = mid - 1
         else:
-            return None
+            high = mid - 1
+
+    print("returning -1")
+    return -1
 
 
 if __name__ == "__main__":
     print("Main function")
     numbers = [1,2,3,4,5,6,7,8,9,10]
-    result = binary_search(numbers, 10)
-    if not None :
-        print(f"Number found at {result}")
-    else:
+    result = binary_search(numbers, 11)
+    if result == -1:
         print("Not FOund")
+    else:
+        print(f"Number found at {result}")
